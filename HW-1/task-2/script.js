4c9440c741d609cd8204051e298721eb11414ab5
g.zubenko <g.zubenko@ukr.net>
2025-02-05 15:25:56 +0200

    initialization block was deleted

--- HW-1/task-2/script.js
+++ HW-1/task-2/script.js
@@ -1,11 +1,9 @@
 // Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. .
 // З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 
-{
-    let firstName = 'Галина';
-    let middleName = 'Андріївна';
-    let lastName = 'Зубенко';
+let firstName = 'Галина';
+let middleName = 'Андріївна';
+let lastName = 'Зубенко';
 
-    let person = `${lastName} ${firstName} ${middleName}`;
-    console.log(person);
-}
+let person = `${lastName} ${firstName} ${middleName}`;
+console.log(person);
--- HW-1/task-3/script.js
+++ HW-1/task-3/script.js
@@ -1,10 +1,10 @@
 // За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
 // let a = 100; let b = '100'; let c = true;
 
-{
-    let a = 100; let b = '100'; let c = true;
+let a = 100;
+let b = '100';
+let c = true;
 
-    console.log(`type of ${a} = ${typeof a}`);
-    console.log(`type of \'${b}\' = ${typeof b}`);
-    console.log(`type of ${c} = ${typeof c}`);
-}
+console.log(`type of ${a} = ${typeof a}`);
+console.log(`type of \'${b}\' = ${typeof b}`);
+console.log(`type of ${c} = ${typeof c}`);
