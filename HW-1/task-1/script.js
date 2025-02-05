85469d741b8977c8556bf1c40db4bb64ad16a2cf
g.zubenko <g.zubenko@ukr.net>
2025-02-05 03:23:44 +0200

    HW_1.1 result files were added

---
+++ HW-1/task-1/script.js
@@ -0,0 +1,44 @@
+// Створити змінні. Присвоїти кожному з них значення:
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+
+{
+    let greeting = 'hello';
+    console.log(greeting);
+
+    let okten_web_university_acronym = 'owu';
+    console.log(okten_web_university_acronym);
+
+    let tld = 'com';
+    console.log(tld);
+
+    let lang = 'ua';
+    console.log(lang);
+
+    let one = 1;
+    console.log(one);
+
+    let ten = 10;
+    console.log(ten);
+
+    let smallest_3_digit_number = -999;
+    console.log(smallest_3_digit_number);
+
+    let n = 123;
+    console.log(n);
+
+    const pi = 3.14; // I hope const is appropriate here
+    console.log(pi);
+
+    let f = 2.7;
+    console.log(f);
+
+    let base = 16;
+    console.log(base);
+
+    let b = true;
+    console.log(b);
+
+    let flag = false;
+    console.log(flag);
+}
