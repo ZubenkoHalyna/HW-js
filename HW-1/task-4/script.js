fbd52bb6c0181dfe587991e0a8b0efd7c50915e2
g.zubenko <g.zubenko@ukr.net>
2025-02-05 03:22:43 +0200

    more user-friendly version was implemented

--- HW-1/task-4/script.js
+++ HW-1/task-4/script.js
@@ -1,33 +1,37 @@
 // За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
 // та вивести в консоль
 {
-    let name = prompt('Введіть своє ім\'я').trim();
-    while (!name) {
-        name = prompt('Введіть своє ім\'я (це обов\'язкове поле)').trim();
+    let info = getInfo();
+
+    if (typeof info === 'string')
+        appendText(`⚠️ ${info} Будь ласка, перезавантажте сторінку, щоб спробувати ще раз`);
+    else {
+        msg = `🎉 Вітаю, ${info.name}` + (info.middleName ? ' ' + info.middleName : '') +
+            `, ваш вік - ${info.age}. Дякую за реєстрацію!`;
+        appendText(msg);
+        console.log(msg);
     }
+}
+
+function getInfo() {
+    let name = prompt('Введіть своє ім\'я (це обов\'язкове поле)').trim();
+    if (!name) return "Ім\'я не може бути пустим.";
     name = name[0].toUpperCase() + name.slice(1);
 
-    let middleName;
-    let confirmation = false;
-    do {
-        middleName = prompt('Введіть своє по-батькові (якщо є)').trim();
-        if (!middleName)
-            confirmation = 'не маю' ===
-                prompt('Якщо ви не маєте по-батькові за документами, введіть "не маю" для підтвердження')
-                    .trim().toLowerCase();
-    } while (!middleName && !confirmation);
+    let middleName = prompt('Введіть своє по-батькові (якщо є)').trim();
+    if (!middleName && 'не маю' !==
+        prompt('Якщо Ви не маєте по-батькові за документами, введіть "не маю" для підтвердження').trim())
+        return "Сталася помилка при введенні по-батькові.";
     if (middleName)
         middleName = middleName[0].toUpperCase() + middleName.slice(1);
 
-    let age;
-    do {
-        let msg = (age === undefined) ? 'Введіть свій вік:' : 'Введіть свій вік цифрами:';
-        let ageString = prompt(msg, '18');
-        age = parseInt(ageString);
-    } while (!age);
+    let age = parseInt(prompt('Введіть свій вік цифрами', 18));
+    if (!age) return "Вік введено не коректно.";
+    return {name: name, middleName: middleName, age: age};
+}
 
-    let output = (middleName) ?
-        `Привіт, ${name} ${middleName}, ваш вік - ${age}. Дякуємо за реєстрацію!`
-        : `Привіт, ${name}, ваш вік - ${age}. Дякуємо за реєстрацію!`;
-    console.log(output);
+function appendText(msg) {
+    let p = document.createElement('p');
+    p.textContent = msg;
+    document.body.append(p);
 }
